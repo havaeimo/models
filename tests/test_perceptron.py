07a931e148d707ea1be53476cf911c634b776758
Marc-Alexandre Côté <marc.cote.19@gmail.com>
2015-08-12 11:33:46 -0400

    BF: Reflect changes made to tasks in smartlearner

--- tests/test_perceptron.py
+++ tests/test_perceptron.py
@@ -6,6 +6,8 @@ from smartmodels.utils import load_mnist
 from smartmodels.utils import Timer
 
 from smartlearner import Trainer, tasks
+from smartlearner.tasks import stopping_criteria
+from smartlearner.tasks import views
 from smartlearner.optimizers import SGD
 from smartlearner.update_rules import ConstantLearningRate
 from smartlearner.losses.classification_losses import NegativeLogLikelihood as NLL
@@ -30,16 +32,16 @@ def test_simple_perceptron():
     with Timer("Building trainer"):
         # Train for 10 epochs
         batch_scheduler = MiniBatchScheduler(trainset, 100)
-        stopping_criterion = tasks.MaxEpochStopping(10)
 
-        trainer = Trainer(optimizer, batch_scheduler, stopping_criterion=stopping_criterion)
+        trainer = Trainer(optimizer, batch_scheduler)
+        trainer.append_task(stopping_criteria.MaxEpochStopping(10))
 
         # Print time for one epoch
         trainer.append_task(tasks.PrintEpochDuration())
         trainer.append_task(tasks.PrintTrainingDuration())
 
         # Print mean/stderror of classification errors.
-        classif_error = tasks.ClassificationError(model.use, validset)
+        classif_error = views.ClassificationError(model.use, validset)
         trainer.append_task(tasks.Print("Validset - Classif error: {0:.1%} ± {1:.1%}", classif_error.mean, classif_error.stderror))
 
     with Timer("Training"):
